c777e8e8ec457c08a73b89b74bb3ed4d14810247
Benjamin Koehler <bkoehler751@gmail.com>
2021-04-06 17:28:39 -0700

    Updates

---
+++ precache-manifest.ce629cc52253fee1a6a62bffad5a812f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2881a019350558b3b77670ee65b4244d",
+    "url": "/breakingbadreact/index.html"
+  },
+  {
+    "revision": "5bb15e4878b50b4832f3",
+    "url": "/breakingbadreact/static/css/main.d1dcdb59.chunk.css"
+  },
+  {
+    "revision": "de2a441ddf4c80ce6609",
+    "url": "/breakingbadreact/static/js/2.8cdb6849.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbadreact/static/js/2.8cdb6849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb15e4878b50b4832f3",
+    "url": "/breakingbadreact/static/js/main.29fd337e.chunk.js"
+  },
+  {
+    "revision": "267444030730ae14c180",
+    "url": "/breakingbadreact/static/js/runtime-main.d1643848.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingbadreact/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbadreact/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breakingbadreact/static/media/spinner.235b9b3e.gif"
+  }
+]);
